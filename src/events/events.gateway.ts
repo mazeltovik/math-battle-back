8f8bd44d06e784b7ed0edeb365b3cdac1fbbf95a
mazeltovik <mazeltovik@gmail.com>
2024-05-05 14:51:00 +0300

    feat: add basic functions

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,36 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  async connect(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    console.log(client.rooms);
+    this.server.emit('message', 'a user connected');
+  }
+  //   @SubscribeMessage('events')
+  //   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+  //     return from([1, 2, 3]).pipe(
+  //       map((item) => ({ event: 'events', data: item })),
+  //     );
+  //   }
+
+  //   @SubscribeMessage('identity')
+  //   async identity(@MessageBody() data: number): Promise<number> {
+  //     return data;
+  //   }
+}
