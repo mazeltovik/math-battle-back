6b6a956f0e4777946ec3254b5fe522629c51d4c9
mazeltovik <mazeltovik@gmail.com>
2024-05-14 10:25:14 +0300

    feat: add basic func

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -7,6 +7,7 @@ import {
   WsResponse,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { from, Observable } from 'rxjs';
 
 @WebSocketGateway({
   cors: {
@@ -22,6 +23,20 @@ export class EventsGateway {
     console.log(client.rooms);
     this.server.emit('message', 'a user connected');
   }
+  @SubscribeMessage('showRooms')
+    handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket): WsResponse<unknown>  {
+      const event = 'showRooms';
+      console.dir(this.server.eventNames());
+      return { event, data:client.rooms };
+  }
+  // @SubscribeMessage('showRooms')
+  // async showRooms(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+  //   const rooms = this.server.of("/").adapter.rooms;
+  //   this.server.of("/").adapter.on("create-room", (room) => {
+  //     console.log(`room ${room} was created`);
+  //   });
+  //   client.emit('showRooms', '');
+  // }
   //   @SubscribeMessage('events')
   //   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
   //     return from([1, 2, 3]).pipe(
